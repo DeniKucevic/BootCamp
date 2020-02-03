e45e847bae941c2c9fc87f8fab02d5c79fd59094
Denis Kucevic <denikucevic@gmail.com>
2020-02-03 22:00:26 +0100

    nedelja2

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,26 @@
+//За два броја(а и б) исписати њихов: Збир, разлику, производ, количник, остатак при дељењу, степен(а ^ b)
+
+let a = 3;
+let b = 4;
+switch (0) {
+    case 0:
+        console.log(a+b)
+        
+    case 1:
+        console.log(a-b)
+        
+    case 2:
+        console.log(a*b)
+        
+    case 3:
+        console.log(a/b)
+        
+    case 4:
+        console.log(a%b)
+        
+    case 5:
+        console.log(a**b)
+        break;
+
+}
+    
---
+++ Nedelja2/zadatak4.js
@@ -0,0 +1,13 @@
+//Исписати првих 1000 бројева дељивих са 5
+
+let i = 0;
+let brojac=0;
+while (brojac <= 1000)
+{
+    i++;
+    if (i%5==0) {
+        console.log(i);
+        brojac++;
+    }
+    else;
+}
