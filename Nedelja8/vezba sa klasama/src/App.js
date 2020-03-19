b15ac98d9d0de98a677f04afaa8912384c07e161
Denis Kucevic <denikucevic@gmail.com>
2020-03-20 00:19:06 +0100

    nedelja8

---
+++ Nedelja8/vezba sa klasama/src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+      
+    constructor(){
+        super();
+        
+        this.state={
+            name:"",
+            number:"",
+            phoneBook:[
+                {name: "Abc", number: "+7898378532"},
+                {name: "Xyz", number: "+9399452757"}
+                ],
+            showForm:true
+           }
+        
+        this.handleInputChange1=(event)=>{
+            this.setState({
+                name:event.target.value
+            })
+        }
+        
+        this.handleInputChange2=(event)=>{
+            this.setState({
+                number:event.target.value
+            })
+        }
+        
+        this.addContact=()=>{
+            
+            const newContact={
+                name:this.state.name,
+                number:this.state.number         
+            }
+            
+            if(this.state.name==="" || this.state.number==="")
+            {
+                alert('Both fields are required.');
+                return;
+            }
+                 
+            this.setState( (prevState)=>({
+                phoneBook:prevState.phoneBook.concat(newContact),
+                 name:"",
+                number:""
+            })) 
+                       
+        }
+                                     
+    }
+
+  render() {
+      
+      let form=null;
+          form=
+             (  
+              <div className="container">
+              <form className="form">
+              <div class="form-group">
+                <input type="text" className="form-control" onChange={this.handleInputChange1} value={this.state.name} placeHolder="Name" />
+              </div>
+              <div class="form-group">
+                <input type="text" className="form-control" onChange={this.handleInputChange2} value={this.state.number} placeHolder="Number"/>
+              </div>
+              <button type="button" className="btn-primary" onClick={this.addContact}>Add</button>
+            </form>
+              </div>
+          )
+           
+    return (
+      <div className="container-fluid">
+      <div className="row">
+      <div className="col-md-4"></div>
+        
+        
+        <div className="col-md-4">
+        <div className="App">
+        <h2 className="header">PhoneBook</h2>
+            
+        {form}
+        
+        {this.state.phoneBook.map(contact =>
+           <div className="contacts">
+           <h5>{contact.name}</h5>
+           <p>{contact.number}</p>
+           <hr/>
+          </div>
+        )} 
+        </div>
+        </div>
+        
+        
+     <div className="col-md-4"></div>
+     </div>
+     </div>
+    );
+  }
+}
+
+export default App;
---
+++ Nedelja8/zadatak1-imenik/src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+
+const App = () => {
+  const [users,setUsers] = useState([])
+  const [name,setName] = useState('')
+  const [number, setNumber] = useState('')
+  const [filtrirani, setFiltrirani] = useState([])
+  const [greska, setGreska] = useState('')
+  const replace = (index) => {
+    let niz = [...users]
+    niz[index].number = number
+    setUsers(niz)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name === '' || number === ''){
+      return setGreska("Popunite sva polja!")
+  }
+   if (!number.startsWith('+')) {
+      return setGreska("Broj mora biti unet u formatu +XXX XXXXXXXXX")
+    }{
+      let index = users.findIndex((user) => user.name === name)
+      if(index !== -1){
+        if(window.confirm('Корисник постоји...Замени број')){
+          replace(index)
+          setGreska("")
+        }
+      }
+      else{
+        setUsers([...users, { name, number }])
+        setGreska("")
+      }
+    }
+    e.target.reset()
+  }
+  const Filter =(e)=>{
+    let filtrirani = users.filter((user) => user.name.includes(e.target.value));
+    setFiltrirani (filtrirani)
+}
+  return (
+    <div className='sve'>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input type="text" placeholder="Unesite ime i prezime" onInput={(e) => setName(e.target.value)}/>
+      <input type="number" placeholder="Unesite broj telefona" onInput={(e) => setNumber(e.target.value)} title="Molimo unesite telefon u formatu +XXXXXXXX"/>
+      <input type="submit" value="Unesi!"/>
+      <input type='text' placeholder='Pretrazi...' onChange={Filter}></input>
+      </form>
+      <div className="greska">
+      {`${greska}`}
+      </div>
+      <div className='svikontakti'>
+      <div className="contacts">
+        {users.map((user) => 
+        <div className="one-contact">
+          <p>{user.name}</p>
+          <p>{user.number}</p>
+          <hr />
+        </div>
+      )}
+      </div>
+      <div className='Filtrirani'>
+        {filtrirani.map((user) =>
+          <>
+        <p>{user.name}</p>
+        <p>{user.number}</p>
+            </>
+          )}
+        </div>
+        </div>
+    </div>
+    )
+}
+
+export default App;
